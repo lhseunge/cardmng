9c6d4864b76b39556f5c32159dfa6c59e02813b7
이현승 <lhseunge@gmail.com>
2023-12-18 19:27:47 +0900

    add controller

---
+++ src/main/java/com/seung/cardmng/controller/CardController.java
@@ -0,0 +1,16 @@
+package com.seung.cardmng.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/v1/card")
+public class CardController {
+
+    @GetMapping
+    public String helloworld() {
+
+        return "hello world";
+    }
+}
