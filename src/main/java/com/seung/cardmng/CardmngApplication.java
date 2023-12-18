b087f1d5ddd34c55728b2c2ade490c26fefe1773
이현승 <lhseunge@gmail.com>
2023-12-18 19:00:07 +0900

    add filesystem

---
+++ src/main/java/com/seung/cardmng/CardmngApplication.java
@@ -0,0 +1,13 @@
+package com.seung.cardmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardmngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardmngApplication.class, args);
+    }
+
+}
