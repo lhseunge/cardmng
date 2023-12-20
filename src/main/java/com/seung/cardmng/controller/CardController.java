29c0ddad673a878e659c8cd992b0f7768d86c417
이현승 <lhseunge@gmail.com>
2023-12-20 16:12:10 +0900

    modify controller annotation

--- src/main/java/com/seung/cardmng/controller/CardController.java
+++ src/main/java/com/seung/cardmng/controller/CardController.java
@@ -1,10 +1,10 @@
 package com.seung.cardmng.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/v1/card")
 public class CardController {
 
