4774edd45da95bf60a5fe75744cb2c56568e7741
이현승 <lhseunge@gmail.com>
2023-12-22 00:10:29 +0900

    add dummy apis

--- src/main/java/com/seung/cardmng/controller/CardController.java
+++ src/main/java/com/seung/cardmng/controller/CardController.java
@@ -1,16 +1,32 @@
 package com.seung.cardmng.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/card")
 public class CardController {
 
-    @GetMapping
+    // @GetMapping
+    @Deprecated
     public String helloworld() {
 
         return "hello world";
+    }
+
+    @GetMapping()
+    public List<String> getCards() {
+
+        return List.of("card 1", "card 2");
+    }
+
+    @GetMapping("/{cardId}")
+    public String getCard(@PathVariable String cardId) {
+
+        return "card 1";
     }
 }
