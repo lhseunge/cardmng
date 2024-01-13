c41268e497dfe3883a325472d48d38b818c1574c
이현승 <lhseunge@gmail.com>
2024-01-14 04:12:57 +0900

    add card service and impl

---
+++ src/main/java/com/seung/cardmng/service/CardService.java
@@ -0,0 +1,9 @@
+package com.seung.cardmng.service;
+
+import java.util.List;
+
+public interface CardService {
+
+    public List<String> getCards();
+
+}
---
+++ src/main/java/com/seung/cardmng/service/impl/CardServiceImpl.java
@@ -0,0 +1,16 @@
+package com.seung.cardmng.service.impl;
+
+import com.seung.cardmng.service.CardService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CardServiceImpl implements CardService {
+
+    @Override
+    public List<String> getCards() {
+        return null;
+    }
+}
+
