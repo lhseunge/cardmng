cbc4ea95292dd6bb897faecb179bea7defd28ae5
이현승 <lhseunge@gmail.com>
2024-02-13 22:33:08 +0900

    modify swagger info

--- src/main/java/com/seung/cardmng/config/SwaggerConfig.java
+++ src/main/java/com/seung/cardmng/config/SwaggerConfig.java
@@ -17,8 +17,8 @@ public class SwaggerConfig {
 
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트")
+                .title("카드 매니저")
+                .description("개인 카드 정보 API")
                 .version("1.0.0");
     }
 }
