5d1d91d14f7c7aaf531a6698b04f405c1891c0c3
이현승 <lhseunge@gmail.com>
2024-02-19 23:54:52 +0900

    add AuthController.java

---
+++ src/main/java/com/seung/cardmng/controller/AuthController.java
@@ -0,0 +1,15 @@
+package com.seung.cardmng.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    public String GenerateToken() {
+        return null;
+    }
+
+    public String googleLoginRedirect() {
+        return null;
+    }
+}
